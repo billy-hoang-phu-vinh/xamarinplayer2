a7dc672d4baba80306b217ef84916680a85db0b1
phu vinh <billyhoang11.ca@gmail.com>
2021-02-24 22:19:19 -0500

    add files

---
+++ MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using MusicApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace a02xamarinplayerapp
+{
+    public partial class MainPage : ContentPage
+    {
+        ObservableCollection<Music> temp_musicList;
+
+        public MainPage(System.Collections.ObjectModel.ObservableCollection<MusicApp.Model.Music> music_list)
+        {
+            InitializeComponent();
+            temp_musicList = music_list;
+            mylist.ItemsSource = music_list;
+            recentMusic = music_list.Where(x => x.IsRecent == true).FirstOrDefault();
+
+        }
+        private Music recentMusic;
+        public Music RecentMusic
+        {
+            get { return recentMusic; }
+            set
+            {
+                recentMusic = value;
+                OnPropertyChanged();
+            }
+        }
+
+        async void mylist_ItemSelected(System.Object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+
+            await Navigation.PushModalAsync(new Play());
+        }
+    }
+}
---
+++ Model/Music.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicApp.Model
+{
+    public class Music
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Url { get; set; }
+        public string CoverImage { get; set; } = "https://raw.githubusercontent.com/billy-hoang-phu-vinh/xamarin-musicplayer/main/sourcefiles/900x520_piano-min.jpg";
+        public bool IsRecent { get; set; }
+    }
+}
